5c325b2eccb7b367e4cc9807490260bccb6a2173
Marcelo Fabri <marcelofabrimf@gmail.com>
2015-06-03 15:05:40 -0300

    Initial Commit

---
+++ DecksetExport/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DecksetExport
+//
+//  Created by Marcelo Fabri on 03/06/15.
+//  Copyright (c) 2015 Marcelo Fabri. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
